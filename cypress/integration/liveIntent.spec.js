c50a37eaea461563f9a3742f05041917586ee2b6
nikhila-godugu1 <70547922+nikhila-godugu1@users.noreply.github.com>
2020-09-01 11:04:31 -0700

    Update liveIntent.spec.js

--- cypress/integration/liveIntent.spec.js
+++ cypress/integration/liveIntent.spec.js
@@ -1,19 +1,13 @@
 import { cyan } from "color-name";
 
 describe('Validate liveIntent Website', () => {
-    it('Navigate to the liveIntent webpage', () => {
+    it('Navigate to the liveIntent webpage and validate Get Started button is displayed', () => {
         cy.visit('https://liveintent.com/');
-    });
-    it('Validate Get Started button is on the page', () => {
         cy.get('a[class*="white-border-btn"]').should('be.visible');
     });
-    it('Click on the Get Started button', () => {
+    it('Click on the Get Started button and validate the navigation', () => {
         cy.get('a[class*="white-border-btn"]').click();
-    });
-    it('Validate the URL to verify the navigation', () => {
         cy.url().should('eq', 'https://www.liveintent.com/get-in-touch/');
-    });
-    it('Validate Get Started button is on the page', () => {
         cy.get('button[class="mktoButton"]').should('be.visible');
     });
 });
