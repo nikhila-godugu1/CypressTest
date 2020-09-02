f97e112a62b8cf6f5c42b77f55bf88c8c095bb45
nikhila-godugu1 <70547922+nikhila-godugu1@users.noreply.github.com>
2020-09-01 18:27:30 -0700

    Fixed load issue

--- cypress/integration/liveIntent.spec.js
+++ cypress/integration/liveIntent.spec.js
@@ -1,12 +1,14 @@
 import { cyan } from "color-name";
 
 describe('Validate liveIntent Website', () => {
-    it('Navigate to the liveIntent webpage and validate Get Started button is displayed', () => {
+    it('Navigate to the LiveIntent webpage and validate Get Started button presence', () => {
         cy.visit('https://liveintent.com/');
-        cy.get('a[class*="white-border-btn"]').should('be.visible');
+        cy.get('a[class*="white-border-btn"]')
+          .should('be.visible')
+          .click();
     });
-    it('Click on the Get Started button and validate the navigation', () => {
-        cy.get('a[class*="white-border-btn"]').click();
+    it('Validate the URL and Contact Us button presence', () => {
+        cy.get('#FirstName',{timeout:30000}).should('have.length', 1)
         cy.url().should('eq', 'https://www.liveintent.com/get-in-touch/');
         cy.get('button[class="mktoButton"]').should('be.visible');
     });
